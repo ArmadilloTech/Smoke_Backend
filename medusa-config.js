eebb40d4167cd09aa275520d51ad53bee8db5a9e
Daman1702 <Beasterboss@gmail.com>
2023-11-30 20:19:51 -0500

    quesionable changes

--- medusa-config.js
+++ medusa-config.js
@@ -47,21 +47,36 @@ const plugins = [
     /** @type {import('@medusajs/admin').PluginOptions} */
     options: {
       autoRebuild: true,
-      // develop: {
-      //   open: process.env.OPEN_BROWSER !== "false",
-      // },
+      develop: {
+        open: process.env.OPEN_BROWSER !== "false",
+      },
     },
   },
 
   { resolve: `medusa-fulfillment-manual` },
   {
-    resolve: `medusa-file-supabase`,
+    resolve: `medusa-file-s3`,
     options: {
-      project_ref: process.env.PROJECT_REF,
-      service_key: process.env.SERVICE_KEY,
-      bucket_name: process.env.BUCKET_NAME,
+      s3_url: process.env.S3_URL,
+      bucket: process.env.S3_BUCKET,
+      region: process.env.S3_REGION,
+      access_key_id: process.env.S3_ACCESS_KEY_ID,
+      secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+      cache_control: process.env.S3_CACHE_CONTROL,
+      // optional
+      download_file_duration: process.env.S3_DOWNLOAD_FILE_DURATION,
+      prefix: process.env.S3_PREFIX,
     },
   },
+
+  // {
+  //   resolve: `medusa-file-supabase`,
+  //   options: {
+  //     project_ref: process.env.PROJECT_REF,
+  //     service_key: process.env.SERVICE_KEY,
+  //     bucket_name: process.env.BUCKET_NAME,
+  //   },
+  // },
   // {
   //   resolve: `medusa-imgur`,
   //   options: {
